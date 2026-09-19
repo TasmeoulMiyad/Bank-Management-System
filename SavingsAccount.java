95da7bf6c7fd341462d3ca987c3d619263d0e617
Shawon chowdhury Setu <shawonchowdhurysetu@gmail.com>
2026-09-19 22:42:26 +0600

    Update SavingsAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,1 +1,25 @@
+public class SavingsAccount extends Account {
+    private double minimumBalance;
 
+    public SavingsAccount(String accountNumber, Customer customer, double initialBalance, double minimumBalance) {
+        super(accountNumber, customer, initialBalance);
+        this.minimumBalance = minimumBalance;
+    }
+
+    public double getMinimumBalance() {
+        return minimumBalance;
+    }
+
+    @Override
+    public void withdraw(double amount) throws Exception {
+        if (amount <= 0) {
+            throw new Exception("Withdrawal amount must be greater than zero.");
+        }
+        if (getBalance() - amount >= minimumBalance) {
+            deductBalance(amount);
+            addTransaction("Withdrawal", amount);
+        } else {
+            throw new Exception("Minimum balance constraint failed! Must retain at least $" + minimumBalance);
+        }
+    }
+}
