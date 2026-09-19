736a7bb94a29c611d3c4176717be1e9885cb31f9
Tasmeoul Miyad <tasmeoul@gmail.com>
2026-09-19 22:15:31 +0600

    Add Customer class with basic attributes and methods

--- Customer.java
+++ Customer.java
@@ -1,1 +1,25 @@
+public class Customer {
+    private String customerId;
+    private String name;
+    private String phone;
+    private String email;
 
+    public Customer(String customerId, String name, String phone, String email) {
+        this.customerId = customerId;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getCustomerId() { return customerId; }
+    public String getName() { return name; }
+    public String getPhone() { return phone; }
+    public String getEmail() { return email; }
+
+    public void setPhone(String phone) { this.phone = phone; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getDisplayInfo() {
+        return "Customer ID: " + customerId + " | Name: " + name + " | Phone: " + phone + " | Email: " + email;
+    }
+}
