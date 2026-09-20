03b9cffc2b8328df135f187cde50c7b45cc07d45
Shawon chowdhury Setu <shawonchowdhurysetu@gmail.com>
2026-09-20 23:14:23 +0600

    Update CurrentAccount.java

--- CurrentAccount.java
+++ CurrentAccount.java
@@ -1,1 +1,25 @@
+public class CurrentAccount extends Account {
+    private double overdraftLimit;
 
+    public CurrentAccount(String accountNumber, Customer customer, double initialBalance, double overdraftLimit) {
+        super(accountNumber, customer, initialBalance);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    public double getOverdraftLimit() {
+        return overdraftLimit;
+    }
+
+    @Override
+    public void withdraw(double amount) throws Exception {
+        if (amount <= 0) {
+            throw new Exception("Withdrawal amount must be greater than zero.");
+        }
+        if (getBalance() - amount >= -overdraftLimit) {
+            deductBalance(amount);
+            addTransaction("Withdrawal", amount);
+        } else {
+            throw new Exception("Overdraft ceiling exceeded! Max allowed overdraft is $" + overdraftLimit);
+        }
+    }
+}
